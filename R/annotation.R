f278d75da43874b23900c51e6798e060cfab8aef
David Zhang <j.zhang@dkfz.de>
2009-01-23 20:44:31 +0000

    add translateKEGGgraph and its helper functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/KEGGgraph@36940 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/annotation.R
+++ R/annotation.R
@@ -18,3 +18,56 @@ translateGeneID2KEGGID <- function(x, organism="hsa") {
 translateKEGG2GeneID <- function(x, organism="hsa") {
   translateKEGGID2GeneID(x, organism=organism)
 }
+
+##------------------------------##
+## Translate the graph - not only the nodes but
+## also the KEGGedge/KEGGnode data
+##------------------------------## 
+## Two 'set' methods will be moved into parse.R or methods.R as
+## soon as they become generics to KEGGgraph. And they will probabily
+## be combined with getKEGGedgeData/getKEGGnodeData
+setKEGGedgeData <- function(graph, list) {
+  env <- new("environment")
+  assign("edges", list, envir=env)
+  edgeDataDefaults(graph, "KEGGEdge") <- env
+  return(graph)
+}
+
+setKEGGnodeData <- function(graph, list) {
+  env <- new("environment")
+  assign("nodes", list, envir=env)
+  nodeDataDefaults(graph, "KEGGNode") <- env
+  return(graph)
+}
+
+translateKEGGgraph <- function(graph, newNodes) {
+  stopifnot(length(nodes(graph)) == length(newNodes))
+  oldNodes <- nodes(graph)
+  nodes(graph) <- newNodes
+
+  ## node
+  knd <- getKEGGnodeData(graph)
+  newNames <- c()
+  for(i in seq(along=knd)) {
+    nN <- newNodes[match( knd[[ i ]]@entryID, oldNodes)]
+    knd[[ i ]]@entryID <- nN
+    knd[[ i ]]@name <- nN
+    newNames[ i ] <- nN
+  }
+  names(knd) <- newNames
+  
+  ## edge
+  ked <- getKEGGedgeData(graph)
+  newNames <- c()
+  for(i in seq(along=ked)) {
+    ked[[ i ]]@entry1ID <- newNodes[match(ked[[ i ]]@entry1ID, oldNodes)]
+    ked[[ i ]]@entry2ID <- newNodes[match(ked[[ i ]]@entry2ID, oldNodes)]
+    newNames[i] <- paste(ked[[ i ]]@entry1ID, ked[[ i ]]@entry2ID, sep="~")
+  }
+  names(ked) <- newNames
+
+  graph <- setKEGGedgeData(graph, ked)
+  graph <- setKEGGnodeData(graph, knd)
+
+  return(graph)
+}
