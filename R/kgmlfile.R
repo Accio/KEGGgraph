3c822730be04a5273c85828455819d1ca4a736c4
David Zhang <j.zhang@dkfz.de>
2017-07-06 06:55:43 +0000

    REST API is now used to get KGML files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/KEGGgraph@130994 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/kgmlfile.R
+++ R/kgmlfile.R
@@ -4,7 +4,7 @@ kgmlNonmetabolicName2MetabolicName <- function(destfile) {
 
 getKGMLurl <- function(pathwayid, organism="hsa") {
   ## baseurl <- "ftp://ftp.genome.jp/pub/kegg/xml/kgml/non-metabolic/organisms"
-  baseurl <- "http://www.genome.jp/kegg-bin/download?entry=%s%s&format=kgml"
+  baseurl <- "http://rest.kegg.jp/get/%s%s/kgml"
 
   pathwayid <- gsub("path","",pathwayid)
   pathwayid <- gsub(":","",pathwayid)
